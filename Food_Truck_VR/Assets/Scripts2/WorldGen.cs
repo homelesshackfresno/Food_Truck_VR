9fa5625033bcc4e1c1771127336c761b3a0c3408
JBaradin <43051720+JBaradin@users.noreply.github.com>
2019-02-23 15:46:41 -0800

    Please Don't Break Anything

---
+++ Food_Truck_VR/Assets/Scripts2/WorldGen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGen : MonoBehaviour
+{
+    public GameObject GO1;
+    public GameObject GO2;
+
+    private float timeElapsed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(GO1, new Vector3(0, 0, 16), Quaternion.identity);
+        Instantiate(GO2, new Vector3(0, 0, 48), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float TimeDelay = 32f / 16f;
+        timeElapsed = timeElapsed + Time.deltaTime;
+        
+
+        if (timeElapsed >= TimeDelay)
+        {
+            timeElapsed = 0f;
+            Instantiate(GO1, new Vector3(0, 0, 48), Quaternion.identity);
+        }
+
+    }
+}
---
+++ Food_Truck_VR/Assets/Scripts2/WorldMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMovement : MonoBehaviour
+{
+    public Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.velocity = new Vector3(0, 0, -16);
+        
+    }
+}
