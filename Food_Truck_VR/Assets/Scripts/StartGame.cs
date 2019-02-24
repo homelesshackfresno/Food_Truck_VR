88d2d92d8a1ffaeaad77d4df71ca445b1a1ce38f
Isaac Caro <isaaccaro24@mail.fresnostate.edu>
2019-02-23 22:28:11 -0800

    added startup script to menu scene

---
+++ Food_Truck_VR/Assets/Scripts/StartGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void Start()
+    {
+        SceneManager.LoadScene("Zach's_Test_Scene");
+    }
+
+}
