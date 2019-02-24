efdd4ee4ffc43f4b1276c9128ecc3cd7123abd51
Isaac Caro <isaaccaro24@mail.fresnostate.edu>
2019-02-24 11:42:01 -0800

    Added Level Start Buttons

---
+++ Food_Truck_VR/Assets/Scripts/StartLv2.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartLv2 : MonoBehaviour
+{
+    public void Start()
+    {
+        SceneManager.LoadScene("Zach's_Test_Scene_2");
+    }
+
+}
