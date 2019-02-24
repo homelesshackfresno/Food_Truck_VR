96e04b9c6cba64a37772007fbea9a7b4c2f66784
ErnestoAlvarado <ernesto.alvarado@ymail.com>
2019-02-23 20:30:30 -0800

    Added a voiceline

---
+++ Food_Truck_VR/Assets/Voice.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voice : MonoBehaviour
+{
+    public AudioClip hey;
+    public AudioSource audio;
+    public float delay = 1.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delay -= Time.deltaTime;
+    }
+
+    void CallOut()
+    {
+        if (delay <= 0)
+        {
+            audio.PlayOneShot(hey, 0.5f);
+        }
+
+        delay = 1.8f;
+    }
+}
